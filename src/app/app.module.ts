8d4d692af8b5425eaebcdcb5710ac7c304d2e003
AlexanderKarpovich <alexander.lomonosov.03@gmail.com>
2023-02-14 15:35:23 +0300

    Error handling added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
+import { GlobalErrorComponent } from './components/global-error/global-error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    GlobalErrorComponent
   ],
   imports: [
     BrowserModule,
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 import { IProduct } from '../models/product';
+import { ErrorService } from './error.service';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class ProductService {
-    constructor(private http: HttpClient) { 
-
-    }
+    constructor(
+        private http: HttpClient,
+        private errorService: ErrorService) { }
 
     getAll(count: number = 0): Observable<IProduct[]> {
         return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
             params: new HttpParams({
                 fromObject: { limit: count }
             })
-        });
+        }).pipe(
+            catchError(this.errorHandler)
+        );
+    }
+
+    private errorHandler(error: HttpErrorResponse) {
+        this.errorService.handle(error.message);
+        return throwError(() => error.message);
     }
 }
